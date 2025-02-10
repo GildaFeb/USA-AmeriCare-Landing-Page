fbdf43fa3b14f26bbf01e6f6458e677d90d4820b
GildaFeb <76148559+GildaFeb@users.noreply.github.com>
2025-02-10 13:33:37 +0800

    Add project files.

---
+++ USA Test Strips Center - Landing Page/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using USA_Test_Strips_Center___Landing_Page.Models;
+
+namespace USA_Test_Strips_Center___Landing_Page.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
